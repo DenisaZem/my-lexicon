fb94be495a06966b6d327596720df01580702efb
DenisaZem <dharibo@seznam.cz>
2024-01-29 12:32:23 +0100

    Fix form_done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,26 @@
 import { useState } from "react"
+import { projectFirestore } from "../firebase/config"
 
 const Form = () => {
     const[wordDe, setWordDe] = useState("")
     const[wordCze, setWordCze] = useState("")
     const[sentence, setSentence] = useState("")
 
-    const submitForm = (e) => {
+    const submitForm = async (e) => {
         e.preventDefault()
 
-        console.log(wordCze)
-        console.log(wordDe)
-        console.log(sentence)
+       const newWord = {wordDe, wordCze, sentence}
+
+       try{
+        await projectFirestore.collection("deutsch").add(newWord)
+
+        setWordDe("")
+        setWordCze("")
+        setSentence("")
+
+       } catch (err) {
+        console.log(err.message)
+       }
     }
 
   return (
@@ -19,18 +29,21 @@ const Form = () => {
             type="text" 
             placeholder="německé slovo"
             onChange={(e) => setWordDe(e.target.value)}
+            value={wordDe}
         />
         <br />
         <input 
             type="text" 
             placeholder="český překlad"
-            onChange={(e) => setWordCze(e.target.value)}    
+            onChange={(e) => setWordCze(e.target.value)}  
+            value={wordCze}  
         />
         <br />
         <input 
             type="text" 
             placeholder="pomocná věta"
             onChange={(e) => setSentence(e.target.value)}
+            value={sentence}
         />
         <br />
         <input 
