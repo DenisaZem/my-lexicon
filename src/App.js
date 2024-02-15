f86c47ca170f25deefcc2a63333cc41c517f9cb8
DenisaZem <dharibo@seznam.cz>
2024-02-15 16:01:25 +0100

    Add NavBar and IntoPage

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import IntroPage from './pages/IntroPage'
 import WordsOverview from './pages/WordsOverview'
 import Form from './components/Form'
 import WordDetail from './pages/WordDetail'
+import LayOut from './components/LayOut'
 
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<IntroPage/>} />
-        <Route path="overview" element={<WordsOverview />} />
-        <Route path="form" element={<Form />} />
-        <Route path="detail-word/:wordId" element={<WordDetail />} />
+          <Route path="/" element={<LayOut/>} >
+          <Route index element={<IntroPage />} />
+          <Route path="overview" element={<WordsOverview />} />
+          <Route path="form" element={<Form />} />
+          <Route path="detail-word/:wordId" element={<WordDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div>
-      NavBar
-    </div>
+    <nav>
+        <ul>
+            <li><Link to="/">Úvod</Link></li>
+            <li><Link to="/overview">Přehled slov</Link></li>
+            <li><Link to="/form">Formulář</Link></li>
+        </ul>
+    </nav>
   )
 }
 
