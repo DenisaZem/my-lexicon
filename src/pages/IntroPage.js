f95adc228574506a12277d483e46a3dfc279ccd7
DenisaZem <dharibo@seznam.cz>
2024-02-15 19:44:55 +0100

    Draft introPage

--- src/pages/IntroPage.js
+++ src/pages/IntroPage.js
@@ -1,9 +1,29 @@
 const IntroPage = () => {
   return (
-    <div>
-      IntroPage
+    <div className="screen">
+    <div className="home">
+      <div className="mainBanner">
+        <div className="wallpaper">
+          <h1>Úvod</h1>
+        </div>
+      </div>
+      <div className="containerBanner">
+        <div className="quote">
+          <p>
+            "Každý den je příležitost naučit se něco nového. Získejte sílu slov a zapamatujte si je s lehkostí."
+          </p>
+        </div>
+      </div>
+      <div className="home-container">
+        <p>
+          Vytvářejte si vlastní paměťové věty a jednoduše si zapamatujte nová slovíčka.
+        </p>
+        <p>
+          Každá věta je klíčem k rozšíření vaší slovní zásoby.
+        </p>
+      </div>
     </div>
-  )
-}
-
+  </div>
+);
+};
 export default IntroPage
