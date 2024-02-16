3c1d445ab0f0d3682719d751ccd9a8ff693ca0e9
DenisaZem <dharibo@seznam.cz>
2024-02-16 12:16:03 +0100

    Navigation added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,29 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import "../images/onlineEdu.png";
 
 const NavBar = () => {
   return (
-    <nav>
-        <ul>
-            <li><Link to="/">Úvod</Link></li>
-            <li><Link to="/overview">Přehled slov</Link></li>
-            <li><Link to="/form">Formulář</Link></li>
-        </ul>
+    <nav className="navBar">
+      <Link to="/">
+        <img
+          src="https://img.freepik.com/free-vector/online-education-cartoon-concept-with-people-learning-online-vector-illustration_1284-84460.jpg?w=1380&t=st=1703592219~exp=1703592819~hmac=fe62f9ff88493f74888e4fb2a22f0425f10da0d31cd47180961cf14ea001a4d9"
+          alt="icon"
+          className="navBar--img_logo"
+        />
+      </Link>
+      <ul className="navBar--list">
+        <li>
+          <Link to="/">Úvod</Link>
+        </li>
+        <li>
+          <Link to="/overview">Přehled slov</Link>
+        </li>
+        <li>
+          <Link to="/form">Formulář</Link>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
