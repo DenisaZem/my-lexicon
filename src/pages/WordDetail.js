0f21d18e88d3afe1f45b2b52df6c918e22a9e5f1
DenisaZem <dharibo@seznam.cz>
2024-02-22 16:35:52 +0100

    WordDetail_styling andcoloring

--- src/pages/WordDetail.js
+++ src/pages/WordDetail.js
@@ -25,20 +25,27 @@ const WordDetail = () => {
   }, [wordId]);
 
   return (
-    <section className="DetailWordTab">
-      {error && <p>{error}</p>}
-      <div className="DetailWordTab__container">
-        <div className="DetailWordTab__wordDe">
-          <h2>{data.wordDe}</h2>
+    <section className="DetailWord">
+      <div className="DetailWordTab__wall">
+        <div className="DetailWordTab__wall_ButtonSection">
+          <button>Přehrát</button>
+          <button>Upravit</button>
+          <button>Zpšt</button>
         </div>
-        <div className="DetailWordTab__wordCze">
-          <h2>{data.wordCze}</h2>
-        </div>
-        <div className="DetailWordTab__title">
-          <span>Příklad:</span>
-        </div>
-        <div className="DetailWordTab__sentence">
-          <h2>{data.sentence}</h2>
+        {error && <p>{error}</p>}
+        <div className="DetailWordTab__container">
+          <div className="DetailWordTab__wordDe">
+            <h2>{data.wordDe}</h2>
+          </div>
+          <div className="DetailWordTab__wordCze">
+            <h2>{data.wordCze}</h2>
+          </div>
+          <div className="DetailWordTab__title">
+            <span>Paměťová věta:</span>
+          </div>
+          <div className="DetailWordTab__sentence">
+            <h2>{data.sentence}</h2>
+          </div>
         </div>
       </div>
     </section>
